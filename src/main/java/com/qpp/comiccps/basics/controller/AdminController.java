160b0e6fe5a6f4721e20fe0497d45df1695e6905
10747 <1074723451@qq.com>
2018-07-09 15:37:46 +0800

    tomcat配置及profile 文件修改2

--- src/main/java/com/qpp/comiccps/basics/controller/AdminController.java
+++ src/main/java/com/qpp/comiccps/basics/controller/AdminController.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
